bd8b0fe9fd7bb022375f2365d1212a81cdad2a1c
pernillasterner <psterner@hotmail.com>
2025-02-18 11:27:50 +0100

    Update techskills

--- src/components/sections/Tech/Tech.jsx
+++ src/components/sections/Tech/Tech.jsx
@@ -4,7 +4,7 @@ import "./Tech.scss";
 
 export const Tech = () => {
   const techStack =
-    "HTML, CSS, Flexbox, Grids, Tailwind, JavaScript, Typescript, ES6, JSX, React.js, React Hooks, Node.js, Mongo DB, Express, Web Accessibility, RestAPIs, mob-programming, pair-programming, GitHub, Agile, UI, Wordpress, PHP, Laravel, TailwindCSS, Alpin.js, Livewire.";
+    "HTML, CSS, Flexbox, Grids, TailwindCSS, JavaScript, Typescript, ES6, JSX, React.js, React Hooks, Node.js, Mongo DB, Express, PHP, Laravel, Alpin.js, Livewire, Web Accessibility, RestAPIs, mob-programming, pair-programming, GitHub, Agile, UI, Wordpress.";
   return (
     <section className="tech-section">
       <div className="tech-container">
